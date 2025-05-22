af4fd1fa22837dc2318a9fd64afc4238538fbb7d
David Rivera <metalpunk321@gmail.com>
2025-05-22 16:23:39 -0700

    chore: move talos cluster config files

--- kustomize/overlays/dev/cluster/talos/plugins/generate-cluster-config.sh
+++ kustomize/overlays/dev/cluster/talos/plugins/generate-cluster-config.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 RESOURCE_LIST=$(cat) # read the `kind: ResourceList` from stdin
-CONF_PATH=$(echo "$RESOURCE_LIST" | yq e '.functionConfig.spec.configPath' - )
-OUTPUT_PATH=$(echo "$RESOURCE_LIST" | yq e '.functionConfig.spec.outputPath' - )
+CONF=($(yq e '.functionConfig.spec.configPaths[]' <<<"$RESOURCE_LIST"))
+OUTPUT_PATH=$(yq e '.functionConfig.spec.outputPath' <<<"$RESOURCE_LIST")
 
-CONF_PATH_GIT="$(git rev-parse --show-prefix)${CONF_PATH}"
-# CONF_HASH=$(git ls-files -co --exclude-standard --full-name ":(glob)${CONF_PATH}/**" | git hash-object --stdin-paths | git hash-object --stdin)
-CONF_HASH=$(find "$PWD/$CONF_PATH" -type f -print | git hash-object --stdin-paths | git hash-object --stdin)
-
-cp -r $CONF_PATH/* $OUTPUT_PATH
+GIT_ROOT=$(git rev-parse --show-toplevel)
+# file/folder renames/moves can change the order of file hashes impacting the final hash.
+# we sort file hashes before generating the final config hash to be consistent.
+CONF_HASH=$(find ${CONF[@]/#/${GIT_ROOT}} -name '*.yaml' -type f -print | git hash-object --stdin-paths | sort -ds | git hash-object --stdin)
 
 cat <<EOF
 kind: ResourceList
@@ -19,6 +18,5 @@ items:
     name: cluster-config-hash
     namespace: talos-system
   data:
-    clusterConfigPath: $CONF_PATH_GIT
     clusterConfigHash: $CONF_HASH
 EOF
